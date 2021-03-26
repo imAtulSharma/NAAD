2d068d4854499bed88f83ca0442d7ce2348088d9
Atul Sharma <atulsharma20may@gmail.com>
2021-03-26 10:49:18 +0530

    sum of two numbers

---
+++ src/com/streamliners/task0/Addition.java
@@ -0,0 +1,19 @@
+package com.streamliners.task0;
+
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+//        initialing scanner to input stream
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter first integer: ");
+        int a = sc.nextInt();
+
+        System.out.println("Please enter second integer: ");
+        int b = sc.nextInt();
+
+//        Printing sum of a and b
+        System.out.println("The sum is  " + a+b);
+    }
+}
